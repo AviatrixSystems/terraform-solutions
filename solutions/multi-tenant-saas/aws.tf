9a10d526c1ec399276210269d50662d40bebe111
Nicolas Delecroix <66386409+ndelecroix@users.noreply.github.com>
2020-06-08 16:36:08 -0700

    Create AWS VGWs, CGWs, site-to-site VPN connections to AVX S2C GWs to simulate customers on-prem environments.

--- solutions/multi-tenant-saas/aws.tf
+++ solutions/multi-tenant-saas/aws.tf
@@ -52,3 +52,32 @@ resource "aws_instance" "test_instances" {
   vpc_security_group_ids      = [aws_security_group.icmp_ssh[each.value.vpc].id]
   key_name                    = each.value.key
 }
+
+resource "aws_vpn_gateway" "vpn_gws" {
+  for_each = var.test_vpn_gateways
+  vpc_id   = aviatrix_vpc.vpcs[each.value.vpc].vpc_id
+  tags = {
+    Name = each.value.name
+  }
+}
+
+resource "aws_customer_gateway" "cgws" {
+  for_each = var.test_customer_gateways
+  bgp_asn  = each.value.bgp_asn
+  # IP address of the corresponding AVX S2C GW
+  ip_address = aviatrix_gateway.customer_s2c_gws[each.value.avx_s2c_gw].eip
+  type       = "ipsec.1"
+
+  tags = {
+    Name = each.value.name
+  }
+}
+
+# For each VGW, create a Site-to-Site VPN connection to AVX S2C GW.
+resource "aws_vpn_connection" "s2s_vpn_connections" {
+  for_each            = var.test_vpn_gateways
+  vpn_gateway_id      = aws_vpn_gateway.vpn_gws[each.key].id
+  customer_gateway_id = aws_customer_gateway.cgws[each.key].id
+  type                = "ipsec.1"
+  static_routes_only  = true
+}
--- solutions/multi-tenant-saas/variables.tf
+++ solutions/multi-tenant-saas/variables.tf
@@ -31,6 +31,20 @@ variable "vpcs" {
       is_transit = false
       is_firenet = false
     }
+    # We use AWS VPCs to simulate customer's on-prem data centers.
+    # They have overlapping IPs on prem - this is on purpose.
+    customer1_on_prem = {
+      name       = "AWS-EW1-SaaS-C1-On-Prem-VPC"
+      cidr       = "192.168.0.0/24"
+      is_transit = false
+      is_firenet = false
+    }
+    customer2_on_prem = {
+      name       = "AWS-EW1-SaaS-C2-On-Prem-VPC"
+      cidr       = "192.168.0.0/24"
+      is_transit = false
+      is_firenet = false
+    }
   }
 }
 
@@ -118,6 +132,36 @@ variable "test_ec2_instances" {
       size                        = "t2.micro"
       key                         = "nicolas"
       associate_public_ip_address = true
+    }
+  }
+}
+
+### We use AWS VGWs to simulate on-prem routers.
+variable "test_vpn_gateways" {
+  default = {
+    customer1 = {
+      name = "C1-On-Prem-VGW"
+      vpc  = "customer1_on_prem"
+    }
+    customer2 = {
+      name = "C2-On-Prem-VGW"
+      vpc  = "customer2_on_prem"
+    }
+  }
+}
+
+### AWS CGWs to represent Aviatrix S2C GWs from AWS VGW perspective.
+variable "test_customer_gateways" {
+  default = {
+    customer1 = {
+      name       = "C1-AVX-S2C-GW-CGW"
+      bgp_asn    = "65000"
+      avx_s2c_gw = "customer1"
+    }
+    customer2 = {
+      name       = "C1-AVX-S2C-GW-CGW"
+      bgp_asn    = "65001"
+      avx_s2c_gw = "customer2"
     }
   }
 }
