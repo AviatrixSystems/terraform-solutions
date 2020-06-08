635e40b1cb0df20cc32fb70c75bc269865c26ddd
Nicolas Delecroix <66386409+ndelecroix@users.noreply.github.com>
2020-06-08 15:31:49 -0700

    Multi-tenant SaaS:   - Enable connected transit on transit GW.   - Attach services and customers spoke GWs to transit GW.   - Create AWS security groups for test instances in every VPC.   - Launch test instances in every VPC.

--- solutions/multi-tenant-saas/multi-tenant-saas.tf
+++ solutions/multi-tenant-saas/multi-tenant-saas.tf
@@ -9,21 +9,40 @@ resource "aviatrix_vpc" "vpcs" {
   aviatrix_firenet_vpc = each.value.is_firenet
 }
 
-### Launch all customer S2C gateways in their respective VPCs.
-resource "aviatrix_gateway" "s2c_gws" {
-  for_each     = var.s2c_gateways
-  cloud_type   = var.cloud_type
-  account_name = var.account_name
-  gw_name      = each.value.name
-  vpc_id       = aviatrix_vpc.vpcs[each.value.customer].vpc_id
-  vpc_reg      = var.region
-  gw_size      = each.value.size
+### Launch the Transit gateway in the Transit VPC.
+resource "aviatrix_transit_gateway" "transit_gw" {
+  cloud_type         = var.cloud_type
+  account_name       = var.account_name
+  gw_name            = var.transit_gateway.name
+  vpc_id             = aviatrix_vpc.vpcs[var.transit_gateway.vpc].vpc_id
+  vpc_reg            = var.region
+  gw_size            = var.transit_gateway.size
+  connected_transit  = true
+  enable_active_mesh = var.transit_gateway.active_mesh
+  single_az_ha       = var.transit_gateway.single_az_ha
+  # subnets[4] is the first public subnet of a Transit VPC, i.e., AZ-a.
+  subnet = aviatrix_vpc.vpcs[var.transit_gateway.vpc].subnets[4].cidr
+}
+
+### Launch the shared services Spoke gateway.
+### Attach it to the Transit gateway.
+resource "aviatrix_spoke_gateway" "services_spoke_gw" {
+  cloud_type         = var.cloud_type
+  account_name       = var.account_name
+  gw_name            = var.services_spoke_gateway.name
+  vpc_id             = aviatrix_vpc.vpcs[var.services_spoke_gateway.vpc].vpc_id
+  vpc_reg            = var.region
+  gw_size            = var.services_spoke_gateway.size
+  enable_active_mesh = var.services_spoke_gateway.active_mesh
+  single_az_ha       = var.services_spoke_gateway.single_az_ha
   # subnets[3] is the first public subnet of a Spoke VPC, i.e., AZ-a.
-  subnet = aviatrix_vpc.vpcs[each.value.customer].subnets[3].cidr
+  subnet     = aviatrix_vpc.vpcs[var.services_spoke_gateway.vpc].subnets[3].cidr
+  transit_gw = var.transit_gateway.name
 }
 
 ### Launch all customer Spoke gateways in their respective VPCs.
-resource "aviatrix_spoke_gateway" "spoke_gws" {
+### Attach them to the Transit gateway.
+resource "aviatrix_spoke_gateway" "customer_spoke_gws" {
   for_each           = var.spoke_gateways
   cloud_type         = var.cloud_type
   account_name       = var.account_name
@@ -34,19 +53,19 @@ resource "aviatrix_spoke_gateway" "spoke_gws" {
   enable_active_mesh = each.value.active_mesh
   single_az_ha       = each.value.single_az_ha
   # subnets[3] is the first public subnet of a Spoke VPC, i.e., AZ-a.
-  subnet = aviatrix_vpc.vpcs[each.value.customer].subnets[3].cidr
+  subnet     = aviatrix_vpc.vpcs[each.value.customer].subnets[3].cidr
+  transit_gw = var.transit_gateway.name
 }
 
-### Launch the Transit gateway in the Transit VPC.
-resource "aviatrix_transit_gateway" "transit_gw" {
-  cloud_type         = var.cloud_type
-  account_name       = var.account_name
-  gw_name            = var.transit_gateway.name
-  vpc_id             = aviatrix_vpc.vpcs[var.transit_gateway.vpc].vpc_id
-  vpc_reg            = var.region
-  gw_size            = var.transit_gateway.size
-  enable_active_mesh = var.transit_gateway.active_mesh
-  single_az_ha       = var.transit_gateway.single_az_ha
-  # subnets[4] is the first public subnet of a Transit VPC, i.e., AZ-a.
-  subnet = aviatrix_vpc.vpcs[var.transit_gateway.vpc].subnets[4].cidr
+### Launch all customer S2C gateways in their respective VPCs.
+resource "aviatrix_gateway" "customer_s2c_gws" {
+  for_each     = var.s2c_gateways
+  cloud_type   = var.cloud_type
+  account_name = var.account_name
+  gw_name      = each.value.name
+  vpc_id       = aviatrix_vpc.vpcs[each.value.customer].vpc_id
+  vpc_reg      = var.region
+  gw_size      = each.value.size
+  # subnets[3] is the first public subnet of a Spoke VPC, i.e., AZ-a.
+  subnet = aviatrix_vpc.vpcs[each.value.customer].subnets[3].cidr
 }
--- solutions/multi-tenant-saas/variables.tf
+++ solutions/multi-tenant-saas/variables.tf
@@ -1,5 +1,5 @@
 variable "account_name" { default = "aws-dev" }
-variable "region" { default = "eu-north-1" }
+variable "region" { default = "eu-west-1" }
 
 ### AWS = 1, GCP = 4, Azure = 8, OCI = 16.
 variable "cloud_type" { default = 1 }
@@ -8,28 +8,70 @@ variable "cloud_type" { default = 1 }
 variable "vpcs" {
   default = {
     saas_transit = {
-      name       = "AWS-EN1-SaaS-Transit-VPC"
+      name       = "AWS-EW1-SaaS-Transit-VPC"
       cidr       = "10.60.0.0/16"
       is_transit = true
       is_firenet = false
     }
     saas_services = {
-      name       = "AWS-EN1-SaaS-Services-VPC"
+      name       = "AWS-EW1-SaaS-Services-VPC"
       cidr       = "10.61.0.0/16"
       is_transit = false
       is_firenet = false
     }
     customer1 = {
-      name       = "AWS-EN1-SaaS-C1-VPC"
+      name       = "AWS-EW1-SaaS-C1-VPC"
       cidr       = "10.62.0.0/16"
       is_transit = false
       is_firenet = false
     }
     customer2 = {
-      name       = "AWS-EN1-SaaS-C2-VPC"
+      name       = "AWS-EW1-SaaS-C2-VPC"
       cidr       = "10.63.0.0/16"
       is_transit = false
       is_firenet = false
+    }
+  }
+}
+
+### Transit gateway.
+variable "transit_gateway" {
+  default = {
+    name         = "AWS-EW1-SaaS-Transit-GW"
+    size         = "t3.small"
+    active_mesh  = true
+    single_az_ha = true
+    vpc          = "saas_transit"
+  }
+}
+
+### Shared services spoke gateway.
+variable "services_spoke_gateway" {
+  default = {
+    name         = "AWS-EW1-Saas-Shared-Services-GW"
+    size         = "t3.small"
+    active_mesh  = true
+    single_az_ha = true
+    vpc          = "saas_services"
+  }
+}
+
+### Customer Spoke gateways.
+variable "spoke_gateways" {
+  default = {
+    customer1 = {
+      customer     = "customer1"
+      name         = "AWS-EW1-SaaS-C1-Spoke-GW"
+      size         = "t3.small"
+      active_mesh  = true
+      single_az_ha = true
+    }
+    customer2 = {
+      customer     = "customer2"
+      name         = "AWS-EW1-SaaS-C2-Spoke-GW"
+      size         = "t3.small"
+      active_mesh  = true
+      single_az_ha = true
     }
   }
 }
@@ -39,44 +81,43 @@ variable "s2c_gateways" {
   default = {
     customer1 = {
       customer = "customer1"
-      name     = "AWS-EN1-SaaS-C1-S2C-GW"
+      name     = "AWS-EW1-SaaS-C1-S2C-GW"
       size     = "t3.small"
     }
     customer2 = {
       customer = "customer2"
-      name     = "AWS-EN1-SaaS-C2-S2C-GW"
+      name     = "AWS-EW1-SaaS-C2-S2C-GW"
       size     = "t3.small"
     }
   }
 }
 
-### Customer Spoke gateways.
-variable "spoke_gateways" {
+### Test EC2 instances.
+variable "test_ec2_instances" {
   default = {
-    customer1 = {
-      customer     = "customer1"
-      name         = "AWS-EN1-SaaS-C1-Spoke-GW"
-      size         = "t3.small"
-      active_mesh  = true
-      single_az_ha = true
+    saas_services_vm = {
+      name                        = "SaaS-Shared-Services-VM"
+      vpc                         = "saas_services"
+      ami                         = "ami-0b4b2d87bdd32212a"
+      size                        = "t2.micro"
+      key                         = "nicolas"
+      associate_public_ip_address = true
     }
-    customer2 = {
-      customer     = "customer2"
-      name         = "AWS-EN1-SaaS-C2-Spoke-GW"
-      size         = "t3.small"
-      active_mesh  = true
-      single_az_ha = true
+    customer1_app_vm = {
+      name                        = "Customer1-App-VM"
+      vpc                         = "customer1"
+      ami                         = "ami-0b4b2d87bdd32212a"
+      size                        = "t2.micro"
+      key                         = "nicolas"
+      associate_public_ip_address = true
     }
-  }
-}
-
-### Transit gateway.
-variable "transit_gateway" {
-  default = {
-    name         = "AWS-EN1-SaaS-Transit-GW"
-    size         = "t3.small"
-    active_mesh  = true
-    single_az_ha = true
-    vpc          = "saas_transit"
+    customer2_app_vm = {
+      name                        = "Customer2-App-VM"
+      vpc                         = "customer2"
+      ami                         = "ami-0b4b2d87bdd32212a"
+      size                        = "t2.micro"
+      key                         = "nicolas"
+      associate_public_ip_address = true
+    }
   }
 }
