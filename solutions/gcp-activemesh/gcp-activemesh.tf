7518e3b4ec34979a59b72fdc0eb0b03fee95c6ea
Nicolas Delecroix <66386409+ndelecroix@users.noreply.github.com>
2020-09-11 01:09:22 +0000

    GCP ActiveMesh

---
+++ solutions/gcp-activemesh/gcp-activemesh.tf
@@ -0,0 +1,101 @@
+provider "aviatrix" {
+  # Make sure to keep the version up to date with the controller version.
+  # https://registry.terraform.io/providers/AviatrixSystems/aviatrix/latest/docs/guides/release-compatibility.
+  version = "~> 2.16.2"
+}
+
+resource "aviatrix_vpc" "gcp_vpcs" {
+  for_each = var.gcp_vpcs
+
+  cloud_type   = 4 # GCP
+  account_name = var.gcp_account_name
+  name         = each.value.name
+
+  subnets {
+    name   = each.value.subnet_name
+    region = each.value.region
+    cidr   = each.value.subnet_cidr
+  }
+}
+
+### GCP Transit gateways.
+resource "aviatrix_transit_gateway" "gcp_bu_transit_gateway" {
+  cloud_type         = 4
+  account_name       = var.gcp_account_name
+  gw_name            = var.gcp_bu_transit_gateway.name
+  vpc_id             = aviatrix_vpc.gcp_vpcs[var.gcp_bu_transit_gateway.vpc].vpc_id
+  vpc_reg            = var.gcp_bu_transit_gateway.zone
+  gw_size            = var.gcp_bu_transit_gateway.size
+  connected_transit  = true
+  enable_active_mesh = var.gcp_bu_transit_gateway.active_mesh
+  single_az_ha       = var.gcp_bu_transit_gateway.single_az_ha
+  subnet             = var.gcp_vpcs[var.gcp_bu_transit_gateway.vpc].subnet_cidr
+  local_as_number    = var.gcp_central_it_transit_gateway.asn
+}
+
+resource "aviatrix_transit_gateway" "gcp_central_it_transit_gateway" {
+  cloud_type         = 4
+  account_name       = var.gcp_account_name
+  gw_name            = var.gcp_central_it_transit_gateway.name
+  vpc_id             = aviatrix_vpc.gcp_vpcs[var.gcp_central_it_transit_gateway.vpc].vpc_id
+  vpc_reg            = var.gcp_central_it_transit_gateway.zone
+  gw_size            = var.gcp_central_it_transit_gateway.size
+  connected_transit  = true
+  enable_active_mesh = var.gcp_central_it_transit_gateway.active_mesh
+  single_az_ha       = var.gcp_central_it_transit_gateway.single_az_ha
+  subnet             = var.gcp_vpcs[var.gcp_central_it_transit_gateway.vpc].subnet_cidr
+  local_as_number    = var.gcp_central_it_transit_gateway.asn
+}
+
+### Peer the Transit gateways.
+resource "aviatrix_transit_gateway_peering" "peering" {
+  transit_gateway_name1 = var.gcp_bu_transit_gateway.name
+  transit_gateway_name2 = var.gcp_central_it_transit_gateway.name
+}
+
+### GCP BU Spoke gateways.
+resource "aviatrix_spoke_gateway" "gcp_bu_spoke_gws" {
+  for_each = var.gcp_bu_spoke_gateways
+
+  cloud_type                       = 4
+  account_name                     = var.gcp_account_name
+  gw_name                          = each.value.name
+  vpc_id                           = aviatrix_vpc.gcp_vpcs[each.value.vpc].vpc_id
+  vpc_reg                          = each.value.zone
+  gw_size                          = each.value.size
+  enable_active_mesh               = each.value.active_mesh
+  single_az_ha                     = each.value.single_az_ha
+  subnet                           = var.gcp_vpcs[each.value.vpc].subnet_cidr
+  included_advertised_spoke_routes = each.value.customized_spoke_vpc_cidr
+  transit_gw                       = each.value.transit_gw
+  depends_on                       = [aviatrix_transit_gateway.gcp_bu_transit_gateway]
+}
+
+### GCP IT Spoke gateways.
+resource "aviatrix_spoke_gateway" "gcp_it_spoke_gws" {
+  for_each = var.gcp_it_spoke_gateways
+
+  cloud_type                       = 4
+  account_name                     = var.gcp_account_name
+  gw_name                          = each.value.name
+  vpc_id                           = aviatrix_vpc.gcp_vpcs[each.value.vpc].vpc_id
+  vpc_reg                          = each.value.zone
+  gw_size                          = each.value.size
+  enable_active_mesh               = each.value.active_mesh
+  single_az_ha                     = each.value.single_az_ha
+  subnet                           = var.gcp_vpcs[each.value.vpc].subnet_cidr
+  included_advertised_spoke_routes = each.value.customized_spoke_vpc_cidr
+  transit_gw                       = each.value.transit_gw
+  depends_on                       = [aviatrix_transit_gateway.gcp_central_it_transit_gateway]
+}
+
+### S2C on Central-IT Transit gateway.
+resource "aviatrix_transit_external_device_conn" "central_it_s2c" {
+  vpc_id            = aviatrix_vpc.gcp_vpcs[var.gcp_central_it_transit_gateway.vpc].vpc_id
+  connection_name   = var.central_it_s2c.name
+  gw_name           = var.gcp_central_it_transit_gateway.name
+  connection_type   = "bgp"
+  bgp_local_as_num  = var.gcp_central_it_transit_gateway.asn
+  bgp_remote_as_num = var.central_it_s2c.remote_asn
+  remote_gateway_ip = var.central_it_s2c.remote_ip
+}
---
+++ solutions/gcp-activemesh/variables.tf
@@ -0,0 +1,113 @@
+variable "gcp_account_name" { default = "gcp-dev" }
+
+### VPCs.
+variable "gcp_vpcs" {
+  default = {
+    bu_transit = {
+      name        = "gcp-bu-transit-vpc"
+      subnet_name = "gcp-bu-transit-subnet"
+      subnet_cidr = "10.160.0.0/24"
+      region      = "us-west2"
+    },
+    bu1_app = {
+      name        = "gcp-bu1-app-shared-vpc"
+      subnet_name = "gcp-bu1-app-spoke-subnet"
+      subnet_cidr = "10.160.2.0/24"
+      region      = "us-west2"
+    },
+    bu2_app = {
+      name        = "gcp-bu2-app-shared-vpc"
+      subnet_name = "gcp-bu2-app-spoke-subnet"
+      subnet_cidr = "10.160.4.0/24"
+      region      = "us-west2"
+    },
+    central_it_transit = {
+      name        = "gcp-central-it-transit-vpc"
+      subnet_name = "gcp-central-it-transit-subnet"
+      subnet_cidr = "10.160.1.0/24"
+      region      = "us-west3"
+    },
+    db = {
+      name        = "gcp-db-vpc"
+      subnet_name = "gcp-db-spoke-subnet"
+      subnet_cidr = "10.160.6.0/24"
+      region      = "us-west3"
+    }
+  }
+}
+
+### GCP Transit gateways.
+variable "gcp_bu_transit_gateway" {
+  default = {
+    name         = "gcp-bu-transit-gw"
+    size         = "n1-standard-1"
+    active_mesh  = true
+    single_az_ha = true
+    vpc          = "bu_transit"
+    zone         = "us-west2-a"
+    asn          = "65160"
+  }
+}
+
+variable "gcp_central_it_transit_gateway" {
+  default = {
+    name         = "gcp-central-it-transit-gw"
+    size         = "n1-standard-1"
+    active_mesh  = true
+    single_az_ha = true
+    vpc          = "central_it_transit"
+    zone         = "us-west3-a"
+    asn          = "65161"
+  }
+}
+
+### GCP BU Spoke gateways.
+variable "gcp_bu_spoke_gateways" {
+  default = {
+    bu1 = {
+      name                      = "gcp-bu1-app-spoke-gw"
+      size                      = "n1-standard-1"
+      active_mesh               = true
+      single_az_ha              = true
+      vpc                       = "bu1_app"
+      zone                      = "us-west2-a"
+      customized_spoke_vpc_cidr = "10.160.2.0/24,10.160.3.0/24"
+      transit_gw                = "gcp-bu-transit-gw"
+    },
+    bu2 = {
+      name                      = "gcp-bu2-app-spoke-gw"
+      size                      = "n1-standard-1"
+      active_mesh               = true
+      single_az_ha              = true
+      vpc                       = "bu2_app"
+      zone                      = "us-west2-a"
+      customized_spoke_vpc_cidr = "10.160.4.0/24,10.160.5.0/24"
+      transit_gw                = "gcp-bu-transit-gw"
+    }
+  }
+}
+
+### GCP IT Spoke gateways.
+variable "gcp_it_spoke_gateways" {
+  default = {
+    db = {
+      name                      = "gcp-db-spoke-gw"
+      size                      = "n1-standard-1"
+      active_mesh               = true
+      single_az_ha              = true
+      vpc                       = "db"
+      zone                      = "us-west3-a"
+      customized_spoke_vpc_cidr = "10.160.6.0/24,10.160.7.0/24"
+      transit_gw                = "gcp-central-it-transit-gw"
+    }
+  }
+}
+
+### Central-IT Transit gateway S2C.
+variable "central_it_s2c" {
+  default = {
+    name       = "gcp-central-it-washington-s2c"
+    remote_asn = "65162"
+    remote_ip  = "54.204.224.4"
+  }
+}
