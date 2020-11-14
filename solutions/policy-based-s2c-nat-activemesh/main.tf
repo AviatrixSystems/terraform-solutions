65c46a6967f93602529e6c06c74cd944bf3a6579
Nicolas Delecroix <66386409+ndelecroix@users.noreply.github.com>
2020-11-14 01:16:47 +0000

    Policy-based S2C with NAT on ActiveMesh Spoke GW

---
+++ solutions/policy-based-s2c-nat-activemesh/main.tf
@@ -0,0 +1,117 @@
+provider "aviatrix" {
+  # Make sure to keep the version up to date with the controller version.
+  # https://registry.terraform.io/providers/AviatrixSystems/aviatrix/latest/docs/guides/release-compatibility.
+  version = "~> 2.17"
+}
+
+provider "aws" {
+  profile = var.aws_profile
+  region  = var.region
+}
+
+resource "aviatrix_vpc" "vpcs" {
+  for_each = var.vpcs
+
+  cloud_type           = var.cloud_type
+  account_name         = var.account_name
+  region               = var.region
+  name                 = each.value.name
+  cidr                 = each.value.cidr
+  aviatrix_transit_vpc = each.value.is_transit
+  aviatrix_firenet_vpc = each.value.is_firenet
+}
+
+data "aws_availability_zones" "az_available" {
+}
+
+### Launch all customer Spoke gateways in their respective VPCs.
+### Attach them to the Transit gateway.
+resource "aviatrix_spoke_gateway" "customer_spoke_gws" {
+  for_each           = var.spoke_gateways
+  cloud_type         = var.cloud_type
+  account_name       = var.account_name
+  gw_name            = each.value.name
+  vpc_id             = aviatrix_vpc.vpcs[each.value.vpc].vpc_id
+  vpc_reg            = var.region
+  gw_size            = each.value.size
+  enable_active_mesh = each.value.active_mesh
+  single_az_ha       = each.value.single_az_ha
+  # The Spoke VPC has been created by the controller. It creates X private
+  # subnets and X public subnets where X is the number of AZs in the region.
+  # We use the first public subnet. Therefore, we index with the number
+  # of AZs in the region to get to the first public subnet.
+  subnet                           = aviatrix_vpc.vpcs[each.value.vpc].subnets[length(data.aws_availability_zones.az_available.names)]["cidr"]
+  transit_gw                       = var.transit_gateway
+  included_advertised_spoke_routes = each.value.customized_spoke_adv_vpc_cidr
+}
+
+### On every Spoke gateway, create S2C connection to on-prem customer.
+resource "aviatrix_site2cloud" "s2c_connections" {
+  for_each = var.s2c_connections
+
+  # vpc_id                     = aviatrix_vpc.vpcs[each.key].vpc_id
+  vpc_id              = aviatrix_vpc.vpcs[each.value.vpc].vpc_id
+  connection_name     = each.value.name
+  connection_type     = "unmapped"
+  remote_gateway_type = "generic"
+  tunnel_type         = "policy"
+  # primary_cloud_gateway_name = var.s2c_gateways[each.key].name
+  primary_cloud_gateway_name = var.spoke_gateways[each.value.avx_spoke_gw].name
+  remote_gateway_ip          = each.value.remote_gw_ip
+  remote_subnet_cidr         = each.value.remote_cidr
+  local_subnet_cidr          = each.value.local_cidr
+  ha_enabled                 = false
+  private_route_encryption   = null
+  custom_algorithms          = true
+  phase_1_authentication     = "SHA-1"
+  phase_2_authentication     = "HMAC-SHA-1"
+  phase_1_dh_groups          = "2"
+  phase_2_dh_groups          = "2"
+  phase_1_encryption         = "AES-128-CBC"
+  phase_2_encryption         = "AES-128-CBC"
+  pre_shared_key             = each.value.psk
+  enable_dead_peer_detection = true
+
+  depends_on = [aviatrix_spoke_gateway.customer_spoke_gws]
+}
+
+### On every Spoke gateway, configure customized SNAT rules.
+### We SNAT on-prem-initiated traffic to the private IP of the GW.
+### Cannot nest for_each statements, so need separate resources for each GW.
+resource "aviatrix_gateway_snat" "customized_snat_spoke4" {
+  gw_name    = var.spoke_gateways["spoke4"].name
+  snat_mode  = "customized_snat"
+  sync_to_ha = false
+
+  dynamic snat_policy {
+    for_each = var.spoke_customized_snat_rules["spoke4"]
+    content {
+      src_cidr   = snat_policy.value.src_cidr
+      dst_cidr   = snat_policy.value.dst_cidr
+      protocol   = snat_policy.value.protocol
+      connection = snat_policy.value.connection
+      # Private IP of the Spoke GW.
+      snat_ips = aviatrix_spoke_gateway.customer_spoke_gws["spoke4"].private_ip
+    }
+  }
+}
+
+### On every Spoke gateway, configure customized DNAT rules.
+### We DNAT cloud-initiated traffic (destined to a virtual IP) to the real
+### IP of the on-prem workload.
+### Cannot nest for_each statements, so need separate resources for each GW.
+resource "aviatrix_gateway_dnat" "customized_dnat_spoke4" {
+  gw_name    = var.spoke_gateways["spoke4"].name
+  sync_to_ha = false
+
+  dynamic dnat_policy {
+    for_each = var.spoke_customized_dnat_rules["spoke4"]
+    content {
+      src_cidr   = dnat_policy.value.src_cidr
+      dst_cidr   = dnat_policy.value.dst_cidr
+      protocol   = dnat_policy.value.protocol
+      connection = dnat_policy.value.connection
+      dnat_ips   = dnat_policy.value.dnat_ip
+    }
+  }
+}
---
+++ solutions/policy-based-s2c-nat-activemesh/variables.tf
@@ -0,0 +1,115 @@
+# From aws configure.
+variable "aws_profile" { default = "default" }
+
+variable "account_name" { default = "aws-dev" }
+variable "region" { default = "us-east-2" }
+
+### AWS = 1, GCP = 4, Azure = 8, OCI = 16.
+variable "cloud_type" { default = 1 }
+
+### VPCs.
+variable "vpcs" {
+  default = {
+    s2c_4 = {
+      name       = "AWS-UE2-S2C-4-VPC"
+      cidr       = "10.7.4.0/24"
+      is_transit = false
+      is_firenet = false
+    },
+    s2c_5 = {
+      name       = "AWS-UE2-S2C-5-VPC"
+      cidr       = "10.7.5.0/24"
+      is_transit = false
+      is_firenet = false
+    }
+  }
+}
+
+variable "transit_gateway" {
+  default = "AWS-UE2-Transit-GW"
+}
+
+### Customer Spoke gateways.
+variable "spoke_gateways" {
+  default = {
+    spoke4 = {
+      vpc          = "s2c_4"
+      name         = "AWS-UE2-Spoke-4-GW"
+      size         = "t3.small"
+      active_mesh  = true
+      single_az_ha = true
+      # VPC CIDR and virtual on-prem subnet
+      customized_spoke_adv_vpc_cidr = "10.7.4.0/24,10.134.0.0/24"
+    },
+    spoke5 = {
+      vpc          = "s2c_5"
+      name         = "AWS-UE2-Spoke-5-GW"
+      size         = "t3.small"
+      active_mesh  = true
+      single_az_ha = true
+      # VPC CIDR and virtual on-prem subnet
+      customized_spoke_adv_vpc_cidr = "10.7.5.0/24,10.135.0.0/24"
+    }
+  }
+}
+
+### Customer S2C connections.
+variable "s2c_connections" {
+  default = {
+    customer4 = {
+      name = "AWS-UE2-C4-Spoke-S2C"
+      # Element from vpcs variable.
+      vpc = "s2c_4"
+      # Element from spoke_gateways variable.
+      avx_spoke_gw = "spoke4"
+      # Public IP of on-prem router.
+      remote_gw_ip = "50.18.221.180"
+      remote_cidr  = "10.34.0.0/24"
+      local_cidr   = "10.1.0.0/16,10.7.4.0/24"
+      psk          = ""
+    },
+    # customer5 = {
+    #   name = "AWS-UE2-C5-Spoke-S2C"
+    #   # Element from vpcs variable.
+    #   vpc = "s2c_5"
+    #   # Element from spoke_gateways variable.
+    #   avx_spoke_gw = "spoke5"
+    #   # Public IP of on-prem router.
+    #   remote_gw_ip = ""
+    #   remote_cidr  = "10.34.0.0/24"
+    #   local_cidr   = "10.1.0.0/16,10.7.5.0/24"
+    #   psk          = ""
+    # }
+  }
+}
+
+### Customized SNAT rules for each Spoke gateway.
+variable "spoke_customized_snat_rules" {
+  default = {
+    spoke4 = {
+      rule1 = {
+        src_cidr   = "10.34.0.0/24" # On-prem
+        dst_cidr   = "10.1.0.0/16"  # Cloud shared services
+        protocol   = "all"
+        interface  = "eth0"
+        connection = "AWS-UE2-Transit-GW" # Post-routing, so outgoing connection
+      }
+    }
+  }
+}
+
+### Customized DNAT rules for each Spoke gateway.
+variable "spoke_customized_dnat_rules" {
+  default = {
+    spoke4 = {
+      rule1 = {
+        src_cidr   = "10.1.0.0/16"    # Cloud shared services 
+        dst_cidr   = "10.134.0.42/32" # Virtual IP of on-prem workload
+        protocol   = "all"
+        interface  = "eth0"
+        connection = "AWS-UE2-Transit-GW" # Pre-routing, so incoming connection
+        dnat_ip    = "10.34.0.42"         # Real IP of on-prem workload
+      }
+    }
+  }
+}
